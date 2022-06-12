97a9ae4323ff78a29ea2c32330daa71f3cad8232
Filippo Sanzani <filippo.sanzani2001@gmail.com>
2022-06-12 23:19:26 +0200

    tag details

--- backend/server/db/services/tag.ts
+++ backend/server/db/services/tag.ts
@@ -1,3 +1,4 @@
+import { isExpressionWithTypeArguments } from "typescript";
 import { TagCreateModel, TagFilterModel } from "../dto/tag";
 import { BaseService } from "./base";
 
@@ -13,6 +14,55 @@ export class TagService extends BaseService {
   getAll(filter: TagFilterModel) {
     return this._prisma.tag.findMany({
       where: filter.getFilterDict()
+    });
+  }
+
+  getViaggi(id: number) {
+    return this._prisma.tag.findUnique({
+      where: {
+        IdTag: id
+      },
+      select: {
+        IdTag: true,
+        Descrizione: true,
+        Descrittori: {
+          select: {
+            Attivita: {
+              select: {
+                Visite: {
+                  select: {
+                    Giornata: {
+                      select: {
+                        Viaggio: true
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }).then(res => ({
+      idTag: res.IdTag,
+      descrizione: res.Descrizione,
+      viaggi: res.Descrittori.flatMap(d => d.Attivita.Visite.flatMap(v => ({
+        idViaggio: v.Giornata.Viaggio.IdViaggio,
+        descrizione: v.Giornata.Viaggio.Descrizione
+      })))
+    })).then(res => {
+      let aggiunti = new Set();
+      let viaggi = [];
+      for(let i=0; i<res.viaggi.length; i++) {
+        if (!aggiunti.has(res.viaggi[i].idViaggio)) {
+          aggiunti.add(res.viaggi[i].idViaggio);
+          viaggi.push(res.viaggi[i]);
+        }
+      }
+      return {
+        ...res,
+        viaggi: viaggi
+      };
     });
   }
 }
